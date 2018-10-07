46ee6c6660646871593a4c23b70fbdb53c452cb6
Vincent REHM <vincentrehm@yahoo.fr>
2018-10-07 19:05:34 +0200

    basic layout for CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :movies
   get 'home/index'
-  get 'home/about'
+  get 'home/about', as: 'about'
   root to: 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_07_125211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "movie_length"
+    t.string "director"
+    t.string "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
